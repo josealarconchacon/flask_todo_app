ee1e1e98d2a0c67a35d8355f8ad1a267096d8f61
Jose Carlos <josealarconchacon@gmail.com>
2020-06-29 13:19:06 -0400

    reading todo item

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# configure flask app to connect to db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://josealarconchacon@localhost:5432/flask_todo_app'
+# link SQLAlchemy to Flask app
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+# debugging statements, define dander-repr method
+def __repr__ (self):
+    return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[{
-        'description': 'Todo 1'
-    }, {
-        'description': 'Todo 2'
-    }, {
-        'description': 'Todo 3'
-    }])
+    return render_template('index.html', data=Todo.query.all())
