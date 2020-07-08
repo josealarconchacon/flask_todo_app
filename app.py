082dd9ea6bc46b3de6604eb29efcfee2ee96a948
Jose Carlos <josealarconchacon@gmail.com>
2020-07-08 11:14:21 -0400

    Many to Many relationship

--- app.py
+++ app.py
@@ -30,6 +30,24 @@ class TodoList(db.Model):
     # specify the relationship
     todos = db.relationship('Todo', backref='list', lazy=True)
 
+
+order_items = db.Table('order_items',
+    db.Column('order_id', db.Integer, db.ForeignKey('order.id'), primary_key=True),
+    db.Column('product_id', db.Integer, db.ForeignKey('product.id'), primary_key=True)
+)
+
+
+class Order(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  status = db.Column(db.String(), nullable=False)
+  products = db.relationship('Product', secondary=order_items,
+      backref=db.backref('orders', lazy=True))
+
+class Product(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
+
+
 # Create
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
